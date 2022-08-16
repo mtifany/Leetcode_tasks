2a206e0bd3d35a80ffa0dbd9132b52439d64d403
mtifany <fed-er-al@yandex.ru>
2022-08-16 19:30:15 +0300

    15-16.08

--- src/ru/leetcode/Leetcode_977_Squares_of_a_Sorted_Array.java
+++ src/ru/leetcode/Leetcode_977_Squares_of_a_Sorted_Array.java
@@ -3,9 +3,21 @@ package ru.leetcode;
 import java.util.Arrays;
 
 public class Leetcode_977_Squares_of_a_Sorted_Array {
-public int[] sortedSquares(int[] nums){
-        return Arrays.stream(nums).map(a -> a * a).sorted().toArray();
-
-}
+        public int[] sortedSquares(int[] A) {
+                int n = A.length;
+                int[] result = new int[n];
+                int i = 0, j = n -1;
+                for(int p = n -1; p>= 0; p--){
+                        if(Math.abs(A[j]) > Math.abs(A[i])) {
+                                result[p] = A[j] * A[j];
+                        j--;
+                        }
+                        else{
+                                result[p] = A[i] * A[i];
+                                i++;
+                        }
+                }
+                return result;
+        }
 
 }
